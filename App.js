712d07c1e4a4b3e2aedb6c0ffd79cbb99fa46bbb
RefanoAndreas2 <62279070+RefanoAndreas2@users.noreply.github.com>
2020-03-17 14:33:54 +0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet, View, SafeAreaView } from 'react-native'
+import { Text } from './components'
+import Navigation from './navigation'
+
+export default function App() {
+  return (
+    <SafeAreaView style={{flex: 1}}>
+      <Navigation/>
+    </SafeAreaView>
+  );
+}
