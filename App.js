3b9cfdef7e7be0267dee83327d092627b12a659f
RefanoAndreas2 <refanoandreas2@gmail.com>
2020-03-17 15:00:19 -0700

    test git

--- App.js
+++ App.js
@@ -3,6 +3,8 @@ import { StyleSheet, View, SafeAreaView } from 'react-native'
 import { Text } from './components'
 import Navigation from './navigation'
 
+// test
+
 export default function App() {
   return (
     <SafeAreaView style={{flex: 1}}>
